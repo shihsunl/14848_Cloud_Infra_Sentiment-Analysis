0616f8183b896dda9bf2277a52d18dc6a5c71abd
bensonlin321 <bensonlin321@gmail.com>
2021-10-01 20:29:20 -0700

    feat: add sa-frontend

---
+++ Sentiment-Analysis/sa-frontend/command.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# downloads all the Javascript dependencies of the React application
+npm install
+# build application into static files. Generates build folder containing all the static files ReactJS application
+npm run build
